07f031f0add0a98110fa16cf289081b87657ced9
zapohd <66849494+zapohd@users.noreply.github.com>
2021-01-04 17:48:10 +0100

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2353,8 +2353,9 @@
     #define Z4_CHAIN_POS     -1
   #endif
 
+  //Change 2021-01-04
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      800
+    #define E0_CURRENT      560
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
