f2999d770ab9b99033dc66fff25bb3aa2650846f
Dan Backe <BAD02@lulea.se>
2021-05-03 14:35:55 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1271,8 +1271,8 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-//Change 2021-04-06
-#define Z_MAX_POS 280
+//Change 2021-05-03
+#define Z_MAX_POS 275
 
 /**
  * Software Endstops
